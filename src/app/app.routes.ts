abe9b334a7e42ead58a52d48b2ace9eb2de89ca3
Breno Kluck <breno.kluck@ottimizza.com.br>
2026-06-19 15:22:47 -0300

    correção de rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     pathMatch: 'full',
     loadComponent: () =>
